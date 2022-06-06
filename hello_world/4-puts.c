587c6f1913be22c3f22ab47f948490e3f420a5a6
root <km@rtin.mobi>
2022-06-05 19:13:45 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,13 @@
-/*
- *file: 4-puts.c
- *auth: klmana
-*/
-
 #include <stdio.h>
 
+/**
+ *main - to print a string
+ *
+ *Return: return status
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
