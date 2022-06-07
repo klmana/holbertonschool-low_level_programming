c057b1c47bcf53572e761cde56b3944ee9df732c
root <km@rtin.mobi>
2022-06-07 00:30:53 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet lowercase without e and q
+*Return: always 0 when success
+*/
+int main(void)
+{
+int letter = 'a';
+while (letter <= 'z')
+{
+if (letter == 'e' | letter == 'q')
+{
+letter += 1;
+}
+else
+{
+putchar(letter);
+letter += 1;
+}
+putchar('\n');
+return (0);
+}
