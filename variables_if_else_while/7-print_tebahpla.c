aeeca13104bf1bfd6d736741871bb2eed519e8ad
root <km@rtin.mobi>
2022-06-07 18:51:39 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - prints the lowercase alphabet in reverse, followed by a new line.
+*Return: always 0 when success
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z'; alphabet >= 'a'; alphabed--)
+{
+putchar (alphabet);
+}
+putchar ('\n')
+return (0);
+}
