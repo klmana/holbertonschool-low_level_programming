56e5f853c97aa8a18fa7b8c8f0183857ecb5f8ca
root <km@rtin.mobi>
2022-06-07 21:11:21 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 int number;
-for (number = 0; number < 9; number++)
+for (number = '0'; number <= '9'; number++)
 {
 putchar(number);
-}
-putchar(',');
+if (number < '9')
 {
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
