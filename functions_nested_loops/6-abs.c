80d88a5ca69e8954427bb0b21a3d443c5a9c6617
root <km@rtin.mobi>
2022-06-09 20:39:01 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stlib.h>
+/**
+*_abs - computes the absolute value of an integer
+*@x: of the appropriate integer type for the function
+*Return: Always 0
+*/
+int _abs(int x)
+{
+	int i = abs(x);
+
+	return (i);
+}
