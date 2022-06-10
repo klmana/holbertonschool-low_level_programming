56d0e61a6743b7d794635723c640e67e12e805e7
root <km@rtin.mobi>
2022-06-10 03:18:09 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - adds two integers and returns the result.
+ *@a: one number
+ *@b: one number
+ *Return: the result
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
