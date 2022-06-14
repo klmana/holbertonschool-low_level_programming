9b29f6534ca267b596c8023c7e5a5f208152a4e5
root <km@rtin.mobi>
2022-06-14 04:02:15 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 != 0)
 		{
-			printf(" fizz");
+			printf(" Fizz");
 		}
 		else if (a % 5 == 0 && a % 3 != 0)
 		{
-			printf(" buzz");
+			printf(" Buzz");
 		}
 		else if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf(" fizzbuzz");
+			printf(" Fizzbuzz");
 		}
 		else if (a == 1)
 		{
