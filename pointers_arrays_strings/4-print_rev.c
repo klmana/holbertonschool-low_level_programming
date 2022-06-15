247eed6fc7b6f40f5b76933e6663e11f35ee3168
root <km@rtin.mobi>
2022-06-15 00:23:54 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- *function - 
- *
+ *print_rev - prints a string, in reverse, followed by a new line
+ *@s:followed by a new line.
  *
  */
-void print
+void print_rev(char *s)
 {
+	int a = 0;
 
+	while (*(s + a) != '\0')
+	{
+		a++;
+	}
+a -= 1;
+while (a >= 0)
+{
+	_putchar(s[a]);
+	a--;
+}
+_putchar('\n');
 }
