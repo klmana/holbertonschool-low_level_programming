f12a2771306bad5dc564e9d42572fbaeeab2b143
root <km@rtin.mobi>
2022-06-14 22:18:44 -0700

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- *function - 
- *
- *
+ *_strlen - function that returns the length of a string.
+ *@s:provides a similar function: strlen
+ *Return: 0
  */
-void print
+int _strlen(char *s)
 {
+	int a;
 
+	for (a = 0; s[a]; a++)
+		;
+	return (a);
 }
