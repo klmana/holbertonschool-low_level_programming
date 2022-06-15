cb39bfb628fc1a653608f16206d101a06585b23a
root <km@rtin.mobi>
2022-06-14 20:00:45 -0700

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- *function - 
- *
+ *reset_to_98 - parameter and updates the value it points to to 98
+ *@n: as parameter and updates the value
  *
  */
-void print
+void reset_to_98(int *n)
 {
+
+	*n = 98;
 
 }
