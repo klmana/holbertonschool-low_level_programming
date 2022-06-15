ac8b2cfc02bb54728fd9e067356a488320f68b12
root <km@rtin.mobi>
2022-06-14 22:48:56 -0700

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
- *function - 
- *
+ *_puts - prints a string, followed by a new line, to stdout
+ *@str: provides a similar function: puts.
  *
  */
-void print
+void _puts(char *str)
 {
+	int a;
 
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
 }
