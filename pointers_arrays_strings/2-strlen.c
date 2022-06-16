cf85bedefc0b802a6fab04dfe672467c1c5b4764
klmana <104742971+klmana@users.noreply.github.com>
2022-06-16 12:28:47 +1000

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,11 @@
  *_strlen - function that returns the length of a string.
  *@s:provides a similar function: strlen
  *Return: 0
+ *we define *s as a pointer to the integer s
+ *we define a variable a that is the length of the string s
+ * s[a] is definining an array of the length of the string of characters defined by s
+ * the for loop takes all the possible values of a from 0 until the end of the arrray done by s
+ *then the function returns the lenghts of s whcih is a
  */
 int _strlen(char *s)
 {
