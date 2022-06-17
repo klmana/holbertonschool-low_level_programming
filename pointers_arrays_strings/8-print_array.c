9494fcf5291377a7b6be46118c6394d6c87ba38b
root <km@rtin.mobi>
2022-06-16 23:16:11 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *function - 
- *
- *
+ *print_array - array of integers, followed by a new line
+ *@a: displayed in the same order as they are stored in the array
+ *@n: elements of the array to be printed
  */
-void print
+void print_array(int *a, int n)
 {
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(" ");
+		i = i + 1;
+	}
+		printf("\n");
 
 }
