181c57eddba1039a5c941b51c33d0afaf84aa8a6
root <km@rtin.mobi>
2022-06-16 19:41:41 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,36 @@
 #include "main.h"
 /**
- *function - 
- *
- *
+ *strlen - return the lenght of s string
+ *@s: character type
+ *Return: i
  */
-void print
+int _strlen(char *s)
 {
+	int i;
 
+	while (s[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return (i);
+}
+
+/**
+ *puts_half - prints half of a string, followed by a new line.
+ *@str:function should print the second half of the string
+ */
+void puts_half(char *str)
+{
+	int n;
+	int len;
+
+	len = _strlen(str);
+	n = ((len - 1) / 2) + 1;
+
+	while (n <= len - 1)
+	{
+		_putchar(str[n]);
+		n = n + 1;
+	}
+	_putchar('\n');
 }
