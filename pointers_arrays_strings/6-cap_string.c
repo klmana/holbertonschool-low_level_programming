a937ec591e35c49ab002552e6efe0ab0f4a78ce8
root <km@rtin.mobi>
2022-06-18 04:23:05 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,30 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
+	int i;
+	int j;
 
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
+	i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 13; j = j + 1)
 		{
 			if (s[i] == spe[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
+					s[i + 1] -= 32;
 
 				}
 			}
 		}
+		i = i + 1;
 	}
 
 	return (s);
