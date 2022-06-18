d81e79c5deee8397fceaf158564048e1e1d2cd0c
root <km@rtin.mobi>
2022-06-18 04:35:25 -0700

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,13 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+
+	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
+		i = i + 1;
 	}
 
 	return (s);
