3dd6dab1d45b8ab88269142d1f95566f6828759a
root <km@rtin.mobi>
2022-06-19 03:53:22 -0700

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strchr - function that locates a character in a string.
+ *@c: pointer to the first occurrence of the character
+ *@s: in the stringpointeR
+ *@NULL: if the character is not found
+ *Return: dest
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (1)
+	{
+		i = *s++;
+		if (i == c)
+		{
+			return (s - 1);
+				}
+		if (i == 0)
+		{
+			return ('\0');
+				}
+	}
+}
