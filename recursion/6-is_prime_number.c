ab4c2019191cd978471b87ea03e0a7a21defa7fe
root <km@rtin.mobi>
2022-06-20 06:04:17 -0700

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
-/**
- * function - 
- * @dest: 
- *
- * Return: 
- */
-returntype function(parameters)
-{
+int actual_prime(int n, int i);
 
+/**
+ * is_prime_number - analyzes if an integer is a prime number or not
+ * @n: number to  parse
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to parse
+ * @i: iteration factor
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
