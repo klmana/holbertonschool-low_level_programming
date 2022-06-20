500ebc2b564905cd7e40a19d99b0eaec27249cad
root <km@rtin.mobi>
2022-06-20 03:42:16 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * function - 
- * @dest: 
- *
- * Return: 
+ *_strlen_recursion - function that returns the length of a string.
+ *@s:length of a string.
+ *Return: 0 if NULL, and lengts of string s otherwise
  */
-returntype function(parameters)
+int _strlen_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 
 }
