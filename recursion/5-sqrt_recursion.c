bc6659e6e69031089ca54966dbf1742851cffb28
root <km@rtin.mobi>
2022-06-20 05:47:50 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,35 @@
 #include "main.h"
 
-/**
- * function - 
- * @dest: 
- *
- * Return: 
- */
-returntype function(parameters)
-{
+int real_sqrt_recursion(int n, int i);
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root
+ *
+ * Return: the actual square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (real_sqrt_recursion(n, 0));
+}
+
+/**
+ * real_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqre root
+ * @i: iterator
+ *
+ * Return: the actual sqr root
+ */
+int real_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (real_sqrt_recursion(n, i + 1));
 }
