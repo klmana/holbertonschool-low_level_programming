e3edf268bef7666dad8f1220dc80fcf44e240abf
root <km@rtin.mobi>
2022-06-21 19:33:02 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Write a program that prints all arguments it receives.
+ *@argc: number of the arguments
+ *@argv: array of the arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+		i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+
+		i = i +1;
+	}
+	return (0);
+}
