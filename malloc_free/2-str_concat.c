1208f30a291d55de24b80447644ca112b512f44f
root <km@rtin.mobi>
2022-06-23 04:33:57 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,33 +10,37 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-char *s3;
-unsigned int i;
-unsigned int len1;
-unsigned int len2;
+	char *s3;
+	unsigned int i;
+	unsigned int len1;
+	unsigned int len2;
 
-len2 = 0;
-len1 = 0;
-i = 0;
+	len2 = 0;
+	len1 = 0;
+	i = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-while (s1[len1])
-	len1 = len1 + 1;
-while (s2[len2])
-	len2 = len2 + 1;
+	while (s1[len1])
+		len1 = len1 + 1;
+	while (s2[len2])
+		len2 = len2 + 1;
 
-s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-if (s3 == NULL)
-	return (NULL);
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
 
-i = 0;
-while (i < (len1 + len2))
-{
-	if (i >= len1)
-		s3[i] = s2[i - len1];
-	else
-		s3[i] = s1[i];
-	i = i + 1;
-}
-s3[i] = '\0';
-return (s3);
+	i = 0;
+	while (i < (len1 + len2))
+	{
+		if (i >= len1)
+			s3[i] = s2[i - len1];
+		else
+			s3[i] = s1[i];
+		i = i + 1;
+	}
+	s3[i] = '\0';
+	return (s3);
 }
