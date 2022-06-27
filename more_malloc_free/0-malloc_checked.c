e6e7c1ade2a6212a268e093047d110a16b8c3f47
root <km@rtin.mobi>
2022-06-26 18:29:14 -0700

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  *malloc_checked - function that allocates memory using malloc.
  *@b: pointer to the allocated memory
- *Returns: a pointer to the allocated memory
+ *Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
