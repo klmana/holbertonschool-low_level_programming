e50d394da51e18224566513dba3e23fca688b6ab
root <km@rtin.mobi>
2022-06-28 18:00:46 -0700

    updated with while loop

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,75 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
-* *string_nconcat - function that concatenates two strings.
-* @s1:first string to concatenate
-* @s2: second string to concatenate
-* @n: selection of number of bytes
-* Return: pointer to the new string created (OK), or KO ( NULL)
-*/
+ *_strlen - function that returns the length of a string.
+ *@s:provides a similar function: strlen
+ *Return: 0
+ *we define *s as a pointer to the integer s
+ *we define a variable a that is the length of the string s
+ *s[a] is definining an array of the length of the string of characters defined
+ *the for loop takes all the possible values of a from 0 until the end of
+ *then the function returns the lenghts of s whcih is an
+ */
+int _strlen(char *s)
+{
+	int a;
+
+a = 0;
+while (s[a] != '\0')
+{
+	a = a + 1;
+}
+return (a);
+}
+
+
+/**
+ *string_nconcat - function that concatenates two strings.
+ *@s1:first string to concatenat
+ *@s2: second string to concatenate
+ *@n: selection of number of bytes
+ *Return: pointer to the new string created (OK), or KO ( NULL)
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	unsigned int i;
-	unsigned int j;
+	unsigned int space1;
+	unsigned int space2;
 	unsigned int len1;
 	unsigned int len2;
 
 	len2 = 0;
 	len1 = 0;
-	i = 0;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[len1])
-		len1 = len1 + 1;
-	while (s2[len2])
-		len2 = len2 + 1;
-	if (n < len2)
-		s3 = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
-		return (NULL);
-	i = 0;
-	for (i = 0; i < len1; i =  i + 1)
-		s3[i] = s1[i];
-	for (j = 0; n < len2 && i < (len1 + n); i = i + 1, j = j + 1)
-		s3[i] = s2[j];
-	for (j = 0; n >= len2 && i < (len1 + len2); i = i + 1, j = j + 1)
-		s3[i] = s2[j];
-	s3[i] = '\0';
-	return (s3);
+	len1 = _strlen(s1);
+
+	len2 = _strlen(s2);
+	if (len2 > n)
+{
+	len2 = n;
+}
+len2 = len1 + len2;
+s3 = malloc(sizeof(*s3) * (len2)+n);
+if (s3 == NULL)
+{
+	return (NULL);
+}
+while (space1 < len1)
+{
+	s3[space1] = s1[space1];
+space1 = space1 + 1;
+}
+while (space1 < len3)
+{
+	s3[space1] = s2[space2];
+space1 = space1 + 1;
+space2 = space2 + 1;
+}
+s3[space1] = '\0';
+return (s3);
 }
