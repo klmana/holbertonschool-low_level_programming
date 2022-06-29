08b293b03335f795de8f2f70faff5a40da015035
root <km@rtin.mobi>
2022-06-28 22:58:39 -0700

    new name for the type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,13 @@ struct dog
 	char *owner;
 };
 
+
+/**
+ * dog_t - new name for the type struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
