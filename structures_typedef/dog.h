5fa0d5227e6dab7a896980feaba9be19905da0bd
root <km@rtin.mobi>
2022-06-28 20:17:27 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Define a new type struct dog with the following elements
+ *@name: The dog name
+ *@age: The dog age
+ *@owner: The owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
