417dc484c435271c8fe2530a7d812557e275c056
root <km@rtin.mobi>
2022-07-03 17:56:34 -0700

    update bracket to nil

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (str == NULL)
-			str = ("nil");
+			str = ("(nil)");
 		if (separator == NULL)
 			printf("%s", str);
 		else if (i == 0)
