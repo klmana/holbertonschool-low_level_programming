0c0107fcb11ede68d5ea738c9394afa755bc7249
root <km@rtin.mobi>
2022-07-18 04:25:13 -0700

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *set_bit - function that sets the value of a bit to 1 at a given index.
+ *@n: number in base 10 format
+ *@index: position int the binary value
+ *Return: 1 if ok  or -1 if an error occured
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int tempIndex;
+	unsigned int max_size;
+
+	/* define the max size of bit taken by n*/
+	max_size = (sizeof(unsigned long int) * 16);
+	/* case if index is over the size of n */
+	if (index > max_size)
+		return (-1);
+	/* use temp index with value 1 to copy value and move to the left */
+	tempIndex = 1;
+	tempIndex <<= index;
+	*n = (*n | tempIndex);
+	return (1);
+}
