57a27de95b0649b426b5f60f3b90c3a359eafc33
root <km@rtin.mobi>
2022-07-17 21:20:36 -0700

    main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int _strlen(char *b);
+int _strlen(const char *b);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
