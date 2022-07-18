40320781d344780d871f693ff5e5f944c78230e7
root <km@rtin.mobi>
2022-07-17 21:32:01 -0700

    task0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_strlen - function that returns the length of a string.
- *bs:provides a similar function: strlen
+ *@b:provides a similar function: strlen
  *Return: 0
  */
 int _strlen(const char *b)
@@ -15,7 +15,7 @@ int _strlen(const char *b)
 
 /**
  *binary_to_uint - function that converts a binary number to an unsigned int
- *@b: the binary
+ *@b:the binary
  *Return: the converted number, or 0 if
  *there is one or more chars in the string b that is not 0 or 1
  *b is NULL
@@ -33,18 +33,18 @@ unsigned int binary_to_uint(const char *b)
 
 	/* case if b is null then value returned should be 0 */
 	if (b == NULL)
-		return (0);
+		return (sum);
 	/* interate while loop to count on the string of char b*/
 	while (b[index] != '\0')
 	{
 		/* case where there is something else than a 0 and 1 in the string */
 		if (b[index] != '0' && b[index] != '1')
-			return (0);
+			return (sum);
 		/* case where there is a 1 in the binary value */
 		if (b[index] == '1')
 			/* if 1 then multiply by power of 2 */
 			/* power of 2 is adding 2 0 to the left of binary */
-			sum += ((1 * (1 << length))) / 2;
+			sum += (1 * (1 << length)) / 2;
 		index++;
 		length--;
 	}
