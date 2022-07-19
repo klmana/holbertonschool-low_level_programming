7527f4b2e0e55975e55c1e748011c2f4be92f824
root <km@rtin.mobi>
2022-07-18 22:37:26 -0700

    put header for close

--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,8 @@
 #include <sys/stat.h>/* to open */
 #include <fcntl.h>
 
+#include <unistd.h>/* to close */
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
