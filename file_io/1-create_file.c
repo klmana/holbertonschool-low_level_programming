a0bc09250047359bc290561344c6a82a4d135289
root <km@rtin.mobi>
2022-07-20 16:22:40 -0700

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	/* create file with permission rw--- (chmod 0600) */
-	forPrint = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	forPrint = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
 	/* case 2.1 file cant be created or writen or fails */
 	if (forPrint == -1)
 		return (-1);
