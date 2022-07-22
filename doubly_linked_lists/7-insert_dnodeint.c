d4703ce93333bbc11bd71f126e2917e69cee1a92
root <km@rtin.mobi>
2022-07-21 17:45:06 -0700

    task7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function that insert a new node at a given position
+ * @h: head of linked list
+ * @idx: index to insert new node
+ * @n: content of the new node
+ * Return: address of new node, or NULL if failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count;
+	dlistint_t *newNode;
+	dlistint_t *interim;
+
+	count = 0;
+
+	/* case with empty list */
+	if (h == NULL)
+		return (NULL);
+	/* allocate memory for new node */
+	newNode = malloc(sizeof(dlistint_t));
+	/* case where the new node is empty */
+	if (newNode == NULL)
+		return (NULL);
+	/* assign temporary value of newNode to n */
+	newNode->n = n;
+	/* case where index is 0 then has to insert newNode as head) */
+	if (idx == 0)
+	{
+		newNode->next = *h;
+		*h = newNode;
+		return (newNode);
+	}
+	/* any other case than the previous ones */
+	/* define the interim/temporary as the head to start indexing */
+	interim = *h;
+	/* start the loop to reach the node before the one to add (indx -1) */
+	while (count < (idx - 1))
+	{
+		interim = interim->next;
+		count++;
+		/* case where the asked node to insert is not in the range */
+		if (interim == NULL)
+		return (NULL);
+	}
+	/* define&link the newNode its place within the list using interim*/
+	newNode->next = interim->next;
+	interim->next = newNode;
+	return (newNode);
+}
