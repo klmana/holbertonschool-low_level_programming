f542e3fd20bcd10a2307cc17c04e4de902033394
root <km@rtin.mobi>
2022-08-17 18:08:33 -0700

    fixing case for the first item in the list

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,7 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp;
 	unsigned int i = 0;
-	char init = 0;
+	char *separator = "";
 
 	if (ht == NULL || ht->array == NULL)
 		return;
@@ -19,10 +19,8 @@ void hash_table_print(const hash_table_t *ht)
 		temp = ht->array[i];
 		while (temp != NULL)
 		{
-			if (init == 1)
-				printf(", ");
-			printf(", '%s': '%s'", temp->key, temp->value);
-			init = 1;
+			printf("%s'%s': '%s'", separator, temp->key, temp->value);
+			separator = ", ";
 			temp = temp->next;
 		}
 		i = i + 1;
