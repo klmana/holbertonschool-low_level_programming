8ab55cd4d6991d51dad351f18b609c6a47758860
root <km@rtin.mobi>
2022-08-17 17:52:33 -0700

    task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: const hash table for which the content has to be printed
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned int i = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+				printf(", '%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+		}
+		i = i + 1;
+	}
+	printf("}\n");
+}
