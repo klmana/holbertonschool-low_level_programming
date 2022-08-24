1e78a62dc02c99fdeb8914cd9d925d594f5ff0ef
root <km@rtin.mobi>
2022-08-24 04:12:01 -0700

    add print_array in binary_search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,6 +1,27 @@
 #include "search_algos.h"
 
 /**
+ *print_array - Prints an array of integers
+ *@array: The array to be printed
+ *@left: index of array
+ *@right: index of array
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t index;
+
+	index = left;
+	while (index <= right)
+	{
+		if (index != right)
+			printf("%d, ", array[index]);
+		else
+			printf("%d\n", array[index]);
+		index = index + 1;
+	}
+}
+
+/**
  *binary_search - function that searches for a value in a sorted array
  *of integers using the binary search algorithm
  *@array: Is a pointer to the first element off the array to search in
@@ -19,6 +40,7 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 	while (left < right)
 	{
+		print_array(array, left, right);
 		midlekey = (left + right) / 2;
 		if (array[midlekey] == value)
 			return (midlekey);
